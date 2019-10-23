45dc5d5248f22c2e4ea3ea0bf82a18c8629af255
muratvuranok <muratvuranok@hotmail.com>
2019-10-23 13:10:24 +0300

    Add project files.

---
+++ HataYonetimi/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace HataYonetimi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnOrnek1 = new System.Windows.Forms.Button();
+            this.btnOrnek2 = new System.Windows.Forms.Button();
+            this.btnOrnek3 = new System.Windows.Forms.Button();
+            this.btnOrnek4 = new System.Windows.Forms.Button();
+            this.btnOrnek5 = new System.Windows.Forms.Button();
+            this.btnOrnek6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(418, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnOrnek1
+            // 
+            this.btnOrnek1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrnek1.Location = new System.Drawing.Point(13, 41);
+            this.btnOrnek1.Name = "btnOrnek1";
+            this.btnOrnek1.Size = new System.Drawing.Size(418, 51);
+            this.btnOrnek1.TabIndex = 1;
+            this.btnOrnek1.Text = "Hata Yönetimsiz";
+            this.btnOrnek1.UseVisualStyleBackColor = true;
+            // 
+            // btnOrnek2
+            // 
+            this.btnOrnek2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrnek2.Location = new System.Drawing.Point(12, 98);
+            this.btnOrnek2.Name = "btnOrnek2";
+            this.btnOrnek2.Size = new System.Drawing.Size(418, 51);
+            this.btnOrnek2.TabIndex = 2;
+            this.btnOrnek2.Text = "Hata Yönetimli";
+            this.btnOrnek2.UseVisualStyleBackColor = true;
+            // 
+            // btnOrnek3
+            // 
+            this.btnOrnek3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrnek3.Location = new System.Drawing.Point(13, 155);
+            this.btnOrnek3.Name = "btnOrnek3";
+            this.btnOrnek3.Size = new System.Drawing.Size(418, 51);
+            this.btnOrnek3.TabIndex = 3;
+            this.btnOrnek3.Text = "Hata Yönetimli - Mesaj";
+            this.btnOrnek3.UseVisualStyleBackColor = true;
+            // 
+            // btnOrnek4
+            // 
+            this.btnOrnek4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrnek4.Location = new System.Drawing.Point(12, 212);
+            this.btnOrnek4.Name = "btnOrnek4";
+            this.btnOrnek4.Size = new System.Drawing.Size(418, 51);
+            this.btnOrnek4.TabIndex = 4;
+            this.btnOrnek4.Text = "Hata Yönetimli - Mesaj";
+            this.btnOrnek4.UseVisualStyleBackColor = true;
+            // 
+            // btnOrnek5
+            // 
+            this.btnOrnek5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrnek5.Location = new System.Drawing.Point(12, 269);
+            this.btnOrnek5.Name = "btnOrnek5";
+            this.btnOrnek5.Size = new System.Drawing.Size(418, 51);
+            this.btnOrnek5.TabIndex = 5;
+            this.btnOrnek5.Text = "Hata Yönetimli - Finally";
+            this.btnOrnek5.UseVisualStyleBackColor = true;
+            // 
+            // btnOrnek6
+            // 
+            this.btnOrnek6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrnek6.Location = new System.Drawing.Point(13, 326);
+            this.btnOrnek6.Name = "btnOrnek6";
+            this.btnOrnek6.Size = new System.Drawing.Size(418, 51);
+            this.btnOrnek6.TabIndex = 6;
+            this.btnOrnek6.Text = "Ekstra";
+            this.btnOrnek6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 392);
+            this.Controls.Add(this.btnOrnek6);
+            this.Controls.Add(this.btnOrnek5);
+            this.Controls.Add(this.btnOrnek4);
+            this.Controls.Add(this.btnOrnek3);
+            this.Controls.Add(this.btnOrnek2);
+            this.Controls.Add(this.btnOrnek1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnOrnek1;
+        private System.Windows.Forms.Button btnOrnek2;
+        private System.Windows.Forms.Button btnOrnek3;
+        private System.Windows.Forms.Button btnOrnek4;
+        private System.Windows.Forms.Button btnOrnek5;
+        private System.Windows.Forms.Button btnOrnek6;
+    }
+}
+
