e8b40c37c0104d34d56c68c22297187cb681b7a3
muratvuranok <muratvuranok@hotmail.com>
2019-10-24 15:03:14 +0300

    Hata Yönetimi

--- HataYonetimi/Form1.Designer.cs
+++ HataYonetimi/Form1.Designer.cs
@@ -34,7 +34,6 @@
             this.btnOrnek3 = new System.Windows.Forms.Button();
             this.btnOrnek4 = new System.Windows.Forms.Button();
             this.btnOrnek5 = new System.Windows.Forms.Button();
-            this.btnOrnek6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -53,6 +52,7 @@
             this.btnOrnek1.TabIndex = 1;
             this.btnOrnek1.Text = "Hata Yönetimsiz";
             this.btnOrnek1.UseVisualStyleBackColor = true;
+            this.btnOrnek1.Click += new System.EventHandler(this.btnOrnek1_Click);
             // 
             // btnOrnek2
             // 
@@ -63,6 +63,7 @@
             this.btnOrnek2.TabIndex = 2;
             this.btnOrnek2.Text = "Hata Yönetimli";
             this.btnOrnek2.UseVisualStyleBackColor = true;
+            this.btnOrnek2.Click += new System.EventHandler(this.btnOrnek2_Click);
             // 
             // btnOrnek3
             // 
@@ -73,6 +74,7 @@
             this.btnOrnek3.TabIndex = 3;
             this.btnOrnek3.Text = "Hata Yönetimli - Mesaj";
             this.btnOrnek3.UseVisualStyleBackColor = true;
+            this.btnOrnek3.Click += new System.EventHandler(this.btnOrnek3_Click);
             // 
             // btnOrnek4
             // 
@@ -83,6 +85,7 @@
             this.btnOrnek4.TabIndex = 4;
             this.btnOrnek4.Text = "Hata Yönetimli - Mesaj";
             this.btnOrnek4.UseVisualStyleBackColor = true;
+            this.btnOrnek4.Click += new System.EventHandler(this.btnOrnek4_Click);
             // 
             // btnOrnek5
             // 
@@ -93,23 +96,13 @@
             this.btnOrnek5.TabIndex = 5;
             this.btnOrnek5.Text = "Hata Yönetimli - Finally";
             this.btnOrnek5.UseVisualStyleBackColor = true;
-            // 
-            // btnOrnek6
-            // 
-            this.btnOrnek6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOrnek6.Location = new System.Drawing.Point(13, 326);
-            this.btnOrnek6.Name = "btnOrnek6";
-            this.btnOrnek6.Size = new System.Drawing.Size(418, 51);
-            this.btnOrnek6.TabIndex = 6;
-            this.btnOrnek6.Text = "Ekstra";
-            this.btnOrnek6.UseVisualStyleBackColor = true;
+            this.btnOrnek5.Click += new System.EventHandler(this.btnOrnek5_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(443, 392);
-            this.Controls.Add(this.btnOrnek6);
+            this.ClientSize = new System.Drawing.Size(443, 332);
             this.Controls.Add(this.btnOrnek5);
             this.Controls.Add(this.btnOrnek4);
             this.Controls.Add(this.btnOrnek3);
@@ -118,7 +111,6 @@
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,7 +124,6 @@
         private System.Windows.Forms.Button btnOrnek3;
         private System.Windows.Forms.Button btnOrnek4;
         private System.Windows.Forms.Button btnOrnek5;
-        private System.Windows.Forms.Button btnOrnek6;
     }
 }
 
--- HataYonetimi/Form1.cs
+++ HataYonetimi/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HataYonetimi
@@ -17,9 +10,84 @@ namespace HataYonetimi
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btnOrnek1_Click(object sender, System.EventArgs e)
         {
-            
+            int telefonNumarasi = Convert.ToInt32(textBox1.Text);
+            MessageBox.Show("Tebrikler telefon numarasını düzgün girdiniz");
+        }
+
+        private void btnOrnek2_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                int telefonNumarasi = Convert.ToInt32(textBox1.Text);
+                MessageBox.Show("Tebrikler telefon numarasını düzgün girdiniz");
+            }
+            catch
+            {
+                MessageBox.Show("Geçerli bir telefon numarası giriniz!");
+            }
+        }
+
+        private void btnOrnek3_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                int telefonNumarasi = Convert.ToInt32(textBox1.Text);
+                MessageBox.Show("Tebrikler telefon numarasını düzgün girdiniz");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Geçerli bir telefon numarası giriniz!");
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnOrnek4_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                int telefonNumarasi = Convert.ToInt32(textBox1.Text);
+                MessageBox.Show("Tebrikler telefon numarasını düzgün girdiniz");
+            }
+            catch (DivideByZeroException ex) // sıfıra bölünme hatası
+            {
+                MessageBox.Show("Geçerli bir telefon numarası giriniz!");
+                MessageBox.Show(ex.Message);
+            }
+            catch (FormatException ex) // geçersiz format 
+            {
+                MessageBox.Show("Geçerli bir telefon numarası giriniz!");
+                MessageBox.Show(ex.Message);
+            }
+            catch (IndexOutOfRangeException ex) //aşırı yüklenme hatası
+            {
+                MessageBox.Show("Geçerli bir telefon numarası giriniz!");
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex) // genel hatalar
+            {
+                MessageBox.Show("Geçerli bir telefon numarası giriniz!");
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnOrnek5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int telefonNumarasi = Convert.ToInt32(textBox1.Text);
+                MessageBox.Show("Tebrikler telefon numarasını düzgün girdiniz");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Geçerli bir telefon numarası giriniz!");
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Her durumda çalışırım bölümü");
+            }
         }
     }
 }
